9795352a3276d63e4550d69ae60d29638201c4a0
Mark Harrop <markharrop@yahoo.com>
2025-08-25 09:00:57 +0100

    tidy

--- lib/streamWrapper.ts
+++ lib/streamWrapper.ts
@@ -1,6 +1,5 @@
 import { Context } from 'hono'
-import { stream, streamSSE } from 'hono/streaming'
-import { StreamingApi } from 'hono/utils/stream'
+import { streamSSE } from 'hono/streaming'
 import { waitOrInterrupt } from './waitOrInterrupt.ts'
 
 export function streamWrapper(
@@ -42,49 +41,6 @@ export function streamWrapper(
     (err, stream) => {
       stream.writeln('An error occurred!')
       console.error('stream error', err)
-      return Promise.resolve()
-    },
-  )
-}
-
-export function streamWrapper2(
-  name: string,
-  c: Context,
-  asyncFunction: (stream: StreamingApi) => Promise<void>,
-  intervalSeconds: number = 60,
-  maxEvents: number = 60,
-) {
-  return stream(
-    c,
-    async (stream) => {
-      let isAborted = false
-
-      stream.onAbort(() => {
-        console.log('Stream aborted!')
-        isAborted = true
-      })
-
-      let counter = 0
-      while (!isAborted && counter < maxEvents) {
-        try {
-          if (!isAborted) {
-            counter++
-            await asyncFunction(stream)
-          }
-          //await new Promise(resolve => setTimeout(resolve, intervalSeconds * 1000))
-          console.log('sleeping for ' + intervalSeconds + ' seconds')
-          await stream.sleep(intervalSeconds * 1000) // Sleep for the specified interval
-          console.log('woke up after ' + intervalSeconds + ' seconds')
-        } catch (error) {
-          console.error('Error in streamWrapper:', error)
-          isAborted = true
-        }
-      }
-      console.log(`Stream ${name} ended after ${counter} events.`)
-    },
-    (err, stream) => {
-      stream.writeln('An error occurred!')
-      console.error(err)
       return Promise.resolve()
     },
   )
